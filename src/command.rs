4c7eb37502e2c788c6ec6421459c45996a6518c7
Akiomi Kamakura <akiomik@gmail.com>
2024-06-02 02:27:29 +0900

    Add book command

--- src/command.rs
+++ src/command.rs
@@ -2,9 +2,28 @@ use clap::{Args, Subcommand};
 
 #[derive(Debug, Subcommand)]
 pub enum Command {
+    /// Manage books
+    #[command(arg_required_else_help = true)]
+    Book(BookArgs),
+
     /// Manage highlights and notes
     #[command(arg_required_else_help = true)]
     Highlight(HighlightArgs),
+}
+
+#[derive(Debug, Args)]
+pub struct BookArgs {
+    #[command(subcommand)]
+    pub command: Option<BookCommand>,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum BookCommand {
+    /// List books
+    List {},
+
+    /// Search books
+    Search { query: String },
 }
 
 #[derive(Debug, Args)]
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 use clap::Parser;
 
-use yel::{Cli, Command, HighlightCommand};
+use yel::{BookCommand, Cli, Command, HighlightCommand};
 
 fn main() {
     let cli = Cli::parse();
 
     match &cli.command {
+        Some(Command::Book(args)) => match &args.command {
+            Some(BookCommand::List {}) => {
+                println!("List");
+            }
+            Some(BookCommand::Search { query }) => {
+                println!("Search with {query}");
+            }
+            None => {}
+        },
         Some(Command::Highlight(args)) => match &args.command {
             Some(HighlightCommand::List {}) => {
                 println!("List");
